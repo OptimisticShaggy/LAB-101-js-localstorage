d050d413c01d7fa72b35dfe1476bc3ec3604f20a
OptimisticShaggy <58822932+OptimisticShaggy@users.noreply.github.com>
2021-02-07 06:40:54 -0500

    progress

--- js/event.js
+++ js/event.js
@@ -4,18 +4,20 @@ const pageStyle = document.getElementsByClassName('pageStyle')
 const userInformation = document.getElementById('userInformation') 
 const light = document.getElementById('light')
 const dark = document.getElementById('dark')
+const visitsWebsite = document.getElementById("numberOfVisits")
 
 ///Gather information for current Session from Local Storage
 let userStorage = window.localStorage.getItem('userStorage')
 let visitorCounter = window.localStorage.getItem('visitorCounter')
 let currentTheme = window.localStorage.getItem('currentTheme')
 
+
 if (userStorage === null || visitorCounter === 0 || currentTheme === null) {
     ///Gather information from User
     userStorage = window.prompt("Could you please tell me your name?").trim()
     currentTheme = window.prompt("Could you type which theme you want? [light] or [dark]").trim()
     
-    ///Permenantly store informaion
+    ///Permenantly store informaion into LocalStorage
     window.localStorage.setItem('userStorage', userStorage)
     window.localStorage.setItem('visitorCounter', 1)
     window.localStorage.setItem('currentTheme', currentTheme)
@@ -24,20 +26,29 @@ if (userStorage === null || visitorCounter === 0 || currentTheme === null) {
     window.localStorage.setItem('visitorCounter', totalVisits)
 }
 
-light.addEventListener('click', function () {
+visitsWebsite.innerHTML = totalVisits
+
+light.addEventListener('click', lightTheme)
+
+dark.addEventListener('click', darkTheme)
+
+const lightTheme = function () {
+    console.log("Test!")
     document.body.setAttribute('class', 'bg-light')
     document.getElementsByTagName('header').setAttribute('class', 'pageStyle col m-5 text-center bg-dark text-light')
     document.getElementsByTagName('main').setAttribute('class', 'pageStyle col m-5 col text-center bg-dark text-light')
     document.getElementById('light').setAttribute('class','btn btn-light')
     document.getElementById('dark').setAttribute('class','btn btn-light')
     document.getElementsByTagName("footer").setAttribute('class', 'pageStyle col m-5 col text-center bg-dark text-light')
-})
+    window.localStorage.setItem('currentTheme', "light")
+}
 
-dark.addEventListener('click', function () {
+const darkTheme = function () {
     document.body.setAttribute('class', 'bg-dark')
     document.getElementsByTagName('header').setAttribute('class', 'pageStyle col m-5 text-center bg-light text-dark')
     document.getElementsByTagName('main').setAttribute('class', 'pageStyle col m-5 col text-center bg-light text-dark')
     document.getElementById('light').setAttribute('class','btn btn-dark')
     document.getElementById('dark').setAttribute('class','btn btn-dark')
     document.getElementsByTagName("footer").setAttribute('class', 'pageStyle col m-5 col text-center bg-light text-dark')
-})
+    window.localStorage.setItem('currentTheme', "dark")
+}
