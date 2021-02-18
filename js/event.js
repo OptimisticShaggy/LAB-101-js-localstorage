43a0b6630eb4c0ba62b59caf1aabff36820860e4
OptimisticShaggy <58822932+OptimisticShaggy@users.noreply.github.com>
2021-02-17 19:32:27 -0500

    Final

--- js/event.js
+++ js/event.js
@@ -1,39 +1,30 @@
 // All of the Starter Variables
-const pageBody = document.getElementById('pageBody')
-const pageStyle = document.getElementsByClassName('pageStyle')
-const userInformation = document.getElementById('userInformation') 
 const light = document.getElementById('light')
 const dark = document.getElementById('dark')
-const visitsWebsite = document.getElementById("numberOfVisits")
-const userName = document.getElementById("nameOfUser")
+const visitsWebsite = document.getElementById('numberOfVisits')
+const userName = document.getElementById('nameOfUser')
 
-///Gather information for current Session from Local Storage
+/// Gather information for current Session from Local Storage
 let userStorage = window.localStorage.getItem('userStorage')
-let visitorCounter = window.localStorage.getItem('visitorCounter')
+const visitorCounter = window.localStorage.getItem('visitorCounter')
 let currentTheme = window.localStorage.getItem('currentTheme')
+let totalVisits = 1
 
 const docHeader = document.getElementsByTagName('header')
 const docMain = document.getElementsByTagName('main')
-const docFooter = document.getElementsByTagName("footer")
+const docFooter = document.getElementsByTagName('footer')
 
-if (userStorage === null || visitorCounter === 0 || visitorCounter === "NaN" || currentTheme === null) {
-    ///Gather information from User
-    userStorage = window.prompt("Could you please tell me your name?").trim()
-    currentTheme = window.prompt("Could you type which theme you want? [light] or [dark]").trim()
-    
-    ///Permenantly store informaion into LocalStorage
-    window.localStorage.setItem('userStorage', userStorage)
-    window.localStorage.setItem('visitorCounter', 1)
-    window.localStorage.setItem('currentTheme', currentTheme)
+if (userStorage === null || visitorCounter === 0 || visitorCounter === 'NaN' || currentTheme === null) {
+  /// Gather information from User
+  userStorage = window.prompt('Could you please tell me your name?').trim()
+  currentTheme = window.prompt('Could you type which theme you want? [light] or [dark]').trim()
+  /// Permenantly store informaion into LocalStorage
+  window.localStorage.setItem('userStorage', userStorage)
+  window.localStorage.setItem('visitorCounter', 1)
+  window.localStorage.setItem('currentTheme', currentTheme)
 } else {
-    totalVisits = 1 + parseInt(visitorCounter)
-    window.localStorage.setItem('visitorCounter', totalVisits)
-}
-
-if (currentTheme == "light") {
-    lightTheme()
-} else if (currentTheme == "dark") {
-    darkTheme()
+  totalVisits = 1 + parseInt(visitorCounter)
+  window.localStorage.setItem('visitorCounter', totalVisits)
 }
 
 userName.innerHTML = userStorage
@@ -41,37 +32,40 @@ visitsWebsite.innerHTML = totalVisits
 light.addEventListener('click', lightTheme)
 dark.addEventListener('click', darkTheme)
 
-function lightTheme() {
-    console.log(currentTheme)
-    document.body.setAttribute('class', 'bg-light')
-    for (var i = 0; i < docHeader.length; i++) {
-        docHeader[i].setAttribute('class', 'pageStyle col m-5 text-center bg-dark text-light')
-    }
-    for (var i = 0; i < docMain.length; i++) {
-        docMain[i].setAttribute('class', 'pageStyle col m-5 col text-center bg-dark text-light')
-    }
-    document.getElementById('light').setAttribute('class','btn btn-light')
-    document.getElementById('dark').setAttribute('class','btn btn-light')
-    for (var i = 0; i < docFooter.length; i++) {
-        docFooter[i].setAttribute('class', 'pageStyle col m-5 col text-center bg-dark text-light')
-    }
-    window.localStorage.setItem('currentTheme', "light")
+if (currentTheme === 'light') {
+  lightTheme()
+} else if (currentTheme === 'dark') {
+  darkTheme()
 }
-    
-function darkTheme() {
-    console.log(currentTheme)
-    document.body.setAttribute('class', 'bg-dark')
 
-    for (var i = 0; i < docHeader.length; i++) {
-        docHeader[i].setAttribute('class', 'pageStyle col m-5 text-center bg-light text-dark')
-    }
-    for (var i = 0; i < docMain.length; i++) {
-        docMain[i].setAttribute('class', 'pageStyle col m-5 col text-center bg-light text-dark')
-    }
-    document.getElementById('light').setAttribute('class','btn btn-dark')
-    document.getElementById('dark').setAttribute('class','btn btn-dark')
-    for (var i = 0; i < docFooter.length; i++) {
-        docFooter[i].setAttribute('class', 'pageStyle col m-5 col text-center bg-light text-dark')
-    }
-    window.localStorage.setItem('currentTheme', "dark")
+function lightTheme () {
+  document.body.setAttribute('class', 'bg-light')
+  for (let ldocHeader = 0; ldocHeader < docHeader.length; ldocHeader++) {
+    docHeader[ldocHeader].setAttribute('class', 'pageStyle col m-5 text-center bg-dark text-light')
+  }
+  for (let ldocMain = 0; ldocMain < docMain.length; ldocMain++) {
+    docMain[ldocMain].setAttribute('class', 'pageStyle col m-5 col text-center bg-dark text-light')
+  }
+  document.getElementById('light').setAttribute('class', 'btn btn-light')
+  document.getElementById('dark').setAttribute('class', 'btn btn-light')
+  for (let idocFooter = 0; idocFooter < docFooter.length; idocFooter++) {
+    docFooter[idocFooter].setAttribute('class', 'pageStyle col m-5 col text-center bg-dark text-light')
+  }
+  window.localStorage.setItem('currentTheme', 'light')
+}
+function darkTheme () {
+  document.body.setAttribute('class', 'bg-dark')
+
+  for (let ddocHeader = 0; ddocHeader < docHeader.length; ddocHeader++) {
+    docHeader[ddocHeader].setAttribute('class', 'pageStyle col m-5 text-center bg-light text-dark')
+  }
+  for (let ddocMain = 0; ddocMain < docMain.length; ddocMain++) {
+    docMain[ddocMain].setAttribute('class', 'pageStyle col m-5 col text-center bg-light text-dark')
+  }
+  document.getElementById('light').setAttribute('class', 'btn btn-dark')
+  document.getElementById('dark').setAttribute('class', 'btn btn-dark')
+  for (let ddocFooter = 0; ddocFooter < docFooter.length; ddocFooter++) {
+    docFooter[ddocFooter].setAttribute('class', 'pageStyle col m-5 col text-center bg-light text-dark')
+  }
+  window.localStorage.setItem('currentTheme', 'dark')
 }
